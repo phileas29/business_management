0c42bc55064695c4afb72a19ef6bd395ae303ffd
Phileas peron <peronphil@mailoo.org>
2023-06-07 21:24:06 +0200

    quarter, semester and year OK

--- solutions/chatGPT/LittleFirmManagement/Controllers/TreasuryController.cs
+++ solutions/chatGPT/LittleFirmManagement/Controllers/TreasuryController.cs
@@ -25,9 +25,8 @@ namespace LittleFirmManagement.Controllers
             _context = context;
         }
 
-        public IActionResult Index(int selectedGranularity = 0)
+        public IActionResult Index(int selectedGranularity = 3)
         {
-
             ViewData["GranularityMapping"] = granularityMapping;
             ViewData["SelectedGranularity"] = selectedGranularity;
 
@@ -43,13 +42,44 @@ namespace LittleFirmManagement.Controllers
                 cashflowIn.Select(c => c.InCreditDate).Max().Value,
                 cashflowOut.Select(c => c.PDebitDate).Max().Value.ToDateTime(new TimeOnly(11, 0))
             };
-            List<DateTime> limitDates = new List<DateTime>() {
+            List<DateTime> limitDatesRaw = new List<DateTime>() {
                 beginDates.Min(),
                 endDates.Max()
             };
 
-            int n = (int)(Math.Pow(2, selectedGranularity+1) - 1);
-            int N = (int)Math.Ceiling( ( ((limitDates[1].Year - limitDates[0].Year) * 12) + limitDates[1].Month - limitDates[0].Month+1 ) / (decimal)n);
+            List<DateTime> limitDatesRounded = new List<DateTime>();
+            for(int i = 0; i < limitDatesRaw.Count; i++)
+            {
+                DateTime roundedDate = new DateTime(limitDatesRaw[i].Year, limitDatesRaw[i].Month, 1, 0, 0, 0);
+                if (i == 1 && 1 < limitDatesRaw[i].Day)
+                    roundedDate = roundedDate.AddMonths(1);
+                limitDatesRounded.Add(roundedDate);
+            }
+            int n = 1;
+            //int n = (int)(Math.Pow(2, selectedGranularity+1) - 1);
+
+            List<DateTime> limitDates = new List<DateTime>();
+            //alignment
+            switch (selectedGranularity)
+            {
+                case 1:
+                    n = 3;
+                    limitDates.Add(limitDatesRounded[0].AddMonths(-((limitDatesRounded[0].Month - 1) % 3)));
+                    limitDates.Add(limitDatesRounded[1].AddMonths(3-((limitDatesRounded[1].Month - 1) % 3 + 3)));
+                    break;
+                case 2:
+                    n = 6;
+                    limitDates.Add(limitDatesRounded[0].AddMonths(-((limitDatesRounded[0].Month - 1) % 6)));
+                    limitDates.Add(limitDatesRounded[1].AddMonths(-((limitDatesRounded[1].Month - 1) % 6 + 6)));
+                    break;
+                case 3:
+                    n = 12;
+                    limitDates.Add(limitDatesRounded[0].AddMonths(-(limitDatesRounded[0].Month - 1)));
+                    limitDates.Add(limitDatesRounded[1].AddYears(1 < limitDatesRounded[1].Month ? 1 : 0));
+                    break;
+            }
+
+            int N = (int)Math.Ceiling( ( ((limitDates[1].Year - limitDates[0].Year) * 12) + limitDates[1].Month - limitDates[0].Month ) / (decimal)n);
 
             List<string> labels = cashflowIn.Select(i => i.label).Union(cashflowOut.Select(p => p.CaName)).ToList();
             int numRows = labels.Count;
